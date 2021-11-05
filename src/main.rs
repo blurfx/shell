4e51bb78709afd9bbfbc595a36bda75cac82338c
Changhui Lee <iam@xo.dev>
2021-11-06 00:40:54 +0900

    feat: support pipe

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io::{stdin, stdout, Write};
-use std::process::Command;
+use std::io::{stdin, stdout};
+use std::process::{Child, Command, Stdio};
 use std::path::Path;
+
 use std::env;
 
 fn main() {
@@ -10,34 +11,53 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut args = input.trim().split_whitespace();
-        if args.clone().count() == 0usize {
-            continue
-        }
-        let command = args.next().unwrap();
+        let mut commands = input.trim().split("|").peekable();
+        let mut prev_command = None;
 
-        match command {
-            "exit" => {
-                return;
+        while let Some(command) = commands.next() {
+            let mut args = command.trim().split_whitespace();
+            if args.clone().count() == 0usize {
+                continue
             }
-            "cd" => {
-                let dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+            let command = args.next().unwrap();
+
+            match command {
+                "exit" => {
+                    return
                 }
-            }
-            command => {
-                let mut command_result = Command::new(command)
-                    .args(args)
-                    .spawn();
-
-                match command_result {
-                    Ok(mut child) => {
-                        child.wait();
-                    },
-                    Err(e) => {
+                "cd" => {
+                    let dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(dir);
+                    if let Err(e) = env::set_current_dir(&root) {
                         eprintln!("{}", e);
+                    }
+                }
+                command => {
+                    let stdin = prev_command.map_or(
+                        Stdio::inherit(),
+                        |output: Child| Stdio::from(output.stdout.unwrap())
+                    );
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(mut output) => {
+                            prev_command = Some(output);
+                        },
+                        Err(e) => {
+                            prev_command = None;
+                            eprintln!("{}", e);
+                        }
                     }
                 }
             }
